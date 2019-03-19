c85e43ff9d1a53af258d2b32c4b39d6e9fc22417
sergey20x25 <vdoond@mail.ru>
2019-03-20 01:37:17 +0700

    remove game logic fron brain-even.js, rename consts

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import { getName, even } from '..';
+import { evenGame } from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".\n');
-
-const name = getName();
-console.log();
-even(name);
+evenGame();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
+const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 };
 
 // brain-even game logic
-const even = (playerName) => {
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+
+  const playerName = readlineSync.question('May I have your name? ');
+  const steps = 3;
+
   const iter = (count) => {
     if (count === 0) {
       console.log(`Congratulations, ${playerName}!`);
       return;
     }
-    const number = Math.round(Math.random() * 99);
-    const rightAnswer = number % 2 === 0 ? 'yes' : 'no';
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === rightAnswer) {
+    const isEven = n => n % 2 === 0;
+    const question = Math.round(Math.random() * 99);
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
+    console.log(`Question: ${question}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer === rightAnswer) {
       console.log('Correct!');
       iter(count - 1);
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${playerName}!`);
     }
   };
-  return iter(3);
+  return iter(steps);
 };
 
-export { getName, even };
+export { greeting, evenGame };

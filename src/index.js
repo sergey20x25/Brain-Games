be354a8ae4b5aca5bcff3a3b5573a3da49a90f17
sergey20x25 <vdoond@mail.ru>
2019-03-20 01:45:59 +0700

    edit brain-games.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
+  console.log('Welcome to the Brain Games!\n');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
 
 // brain-even game logic
